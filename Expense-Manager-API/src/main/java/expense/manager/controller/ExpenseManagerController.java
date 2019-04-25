682f5706efff74c6c33d604ef65dde4bb83965dd
Subhadeep Sen <subhadeep0977@gmail.com>
2019-04-25 23:04:49 +0530

    added swagger for api documentation

--- Expense-Manager-API/src/main/java/expense/manager/controller/ExpenseManagerController.java
+++ Expense-Manager-API/src/main/java/expense/manager/controller/ExpenseManagerController.java
@@ -14,46 +14,71 @@ import expense.manager.model.DailyExpense;
 import expense.manager.model.Expense;
 import expense.manager.model.MonthlyExpense;
 import expense.manager.service.IExpenseManagerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/expense/api")
+@Api
 public class ExpenseManagerController {
 
 	@Autowired
 	private IExpenseManagerService expenseManagerService;
 
-	@RequestMapping(value = "/addDailyExpense", method = RequestMethod.POST)
+	@ApiOperation(value = "Add Daily Expense", response = DailyExpense.class)
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "Content-Type", value = "application/json", required = true, dataType = "string", paramType = "header") })
+	@RequestMapping(value = "/addDailyExpense", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
 	public DailyExpense addDailyExpense(@RequestBody DailyExpense dailyExpense) {
 		return expenseManagerService.addDailyExpense(dailyExpense);
 	}
 
+	@ApiOperation(value = "Set Monthly Budget Limit", response = Boolean.class)
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "month", value = "JANUARY", required = true, dataType = "string", paramType = "path"),
+			@ApiImplicitParam(name = "budgetLimit", value = "0", required = true, dataType = "double", paramType = "path") })
 	@RequestMapping(value = "/setBudgetLimit/{month}/{budgetLimit}", method = RequestMethod.POST)
 	public boolean setMonthlyBadgetLimit(@PathVariable String month, @PathVariable double budgetLimit) {
 		return expenseManagerService.setMonthlyBadgetLimit(month, budgetLimit);
 	}
 
+	@ApiOperation(value = "Set Current Month's Budget Limit", response = Boolean.class)
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "budgetLimit", value = "0", required = true, dataType = "double", paramType = "path") })
 	@RequestMapping(value = "/setBudgetLimit/{budgetLimit}", method = RequestMethod.POST)
 	public boolean setMonthlyBadgetLimit(@PathVariable double budgetLimit) {
 		return expenseManagerService.setMonthlyBadgetLimit("", budgetLimit);
 	}
 
-	@RequestMapping(value = "/retrieveExpensesByDate/{date}", method = RequestMethod.GET)
+	@ApiOperation(value = "Retrieve Daily Expense", response = List.class)
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "date", value = "YYYY-MM-DD", required = true, dataType = "string", paramType = "path") })
+	@RequestMapping(value = "/retrieveExpensesByDate/{date}", method = RequestMethod.GET, produces = "application/json")
 	public List<DailyExpense> retrieveExpensesByDate(@PathVariable String date) {
 		return expenseManagerService.retrieveExpensesByDate(date);
 	}
-	
-	@RequestMapping(value = "/retrieveExpensesByMonth/{month}", method = RequestMethod.GET)
+
+	@ApiOperation(value = "Retrieve Monthly Expense", response = List.class)
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "month", value = "JANUARY", required = true, dataType = "string", paramType = "path") })
+	@RequestMapping(value = "/retrieveExpensesByMonth/{month}", method = RequestMethod.GET, produces = "application/json")
 	public List<MonthlyExpense> retrieveExpensesByMonth(@PathVariable String month) {
 		return expenseManagerService.retrieveExpensesByMonth(month);
 	}
 
-	@RequestMapping(value = "/retrieveExpensesByYear/{year}", method = RequestMethod.GET)
+	@ApiOperation(value = "Retrieve Yearly Expense", response = List.class)
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "year", value = "YYYY", required = true, dataType = "integer", paramType = "path") })
+	@RequestMapping(value = "/retrieveExpensesByYear/{year}", method = RequestMethod.GET, produces = "application/json")
 	public List<Expense> retrieveExpensesByYear(@PathVariable int year) {
 		return expenseManagerService.retrieveExpensesByYear(year);
 	}
 
-	@RequestMapping(value = "/retrieveAllExpenses", method = RequestMethod.GET)
+	@ApiOperation(value = "Retrieve All Expenses", response = List.class)
+	@RequestMapping(value = "/retrieveAllExpenses", method = RequestMethod.GET, produces = "application/json")
 	public List<Expense> retrieveExpenses() {
 		return expenseManagerService.retrieveExpenses();
 	}
